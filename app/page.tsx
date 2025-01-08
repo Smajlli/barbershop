26ba0117b53fca13117c9f73b25b2002fe2a4fcc
Amer Smajlovic <amer.smajli@icloud.com>
2025-01-08 14:58:09 +0100

    Create sing up system for users with supabase client and middleware (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
+  return <>
     <div>This is homepage</div>
-  );
+    <Link href={'/signup'}>Sign up</Link>
+  </>
 }
