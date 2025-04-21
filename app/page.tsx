3deaec2bc3f78df4d85b09ebbdc9729715e369a0
Amer Smajlovic <amer.smajli@icloud.com>
2025-04-21 14:47:03 +0200

    Authentification (#5)
    * Create sign up system for users with supabase client and middleware
    
    * Create login route and implement login functionality
    
    * Install missing packages(webidl-conversions & tr46)
    
    * Check for user session and add basic authorization if there is no session
    
    * redirect after being successfully logged in
    
    * Temporary turn off email verification and redirect after sign up
    
    * Temporary turn off email verification and redirect after sign up
    
    * Add temporary sign out button
    
    * Install react-toastify package

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import React from "react";
+import { createClient } from "./utils/supabase/client";
 
 export default function Home() {
+
+  const logout = async() => {
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signOut()
+
+    if(error) console.log(error);
+  }
 
   return <div className="w-screen h-screen flex items-center justify-center flex-col gap-4">
     <h1 className="text-4xl">Wellcome to barbershop app</h1>
     <button type="button" className="bg-emerald-300 rounded-xl px-3 py-2 text-white">Book your appointment</button>
+    <button type="button" className="bg-emerald-300 rounded-xl px-3 py-2 text-white" onClick={logout}>Sign out</button>
   </div>
 }
