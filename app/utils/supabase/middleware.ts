ea2bb301199ef6d8f39ff7945da7c3b0ce3c8541
Smajlli <amer.smajli@icloud.com>
2025-05-03 16:15:57 +0200

    Add

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -27,7 +27,7 @@ export async function updateSession(request: NextRequest) {
 
     const {data: {user}} = await supabase.auth.getUser();
 
-    if(!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/signup')) {
+    if(!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/signup') && !request.nextUrl.pathname.startsWith('/admin/register')) {
         const url = request.nextUrl.clone()
         url.pathname = '/login'
         return NextResponse.redirect(url)
