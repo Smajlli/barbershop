9ee971cbc20c43e4784652e0fa64cbc8b6be49b3
Amer Smajlovic <amer.smajli@icloud.com>
2025-05-08 14:37:06 +0200

    Display appointments, and booked services for relevant appointment (#9)
    * Display appointments, and booked services for relevant appointment
    
    * Add temporary styles
    
    * Add padding

--- app/admin/appointments/page.tsx
+++ app/admin/appointments/page.tsx
@@ -2,6 +2,7 @@
 
 import { createClient } from "@/app/utils/supabase/client";
 import { useState, useEffect } from "react";
+import { IoMdTime } from "react-icons/io";
 
 type appointmentType = {
     date?: string,
@@ -12,8 +13,18 @@ type appointmentType = {
     user_id?: string
 }
 
+type bookedServicesType = {
+    id: string, 
+    service_name: string, 
+    service_count: number, 
+    user_id: string
+}
+
 function Appointments() {
     const [appointments, setAppointments] = useState<appointmentType[]>();
+    const [bookedServices, setBookedServices] = useState<bookedServicesType[]>();
+    const day = new Date().toString().split(' ')[0];
+    const month = ("0" + (new Date().getMonth() + 1)).slice(-2);
 
     useEffect(() => {
         async function fetchAppointments() {
@@ -25,13 +36,41 @@ function Appointments() {
                 if (error) console.log(error.message)
             }
         }
+
+        async function fetchServices() {
+            const supabase = createClient();
+            const {data, error} = await supabase.from('booked_services').select('*');
+            if(data) setBookedServices(data);
+            if(error) console.log(error);
+        }
         fetchAppointments();
+        fetchServices();
     }, [])
 
     return <div>
-        <h1> Here's appointments for today </h1>
-        <div>{appointments?.map((a) => (
-            <div> {a.fullname} </div>
+        <h1 className="text-4xl"> Appointments </h1>
+        <p className="text-slate-600">See your scheduled appointments below.</p>
+        <div className="flex flex-col gap-4">{appointments?.map((a, i) => (
+            <div key={i} className="w-full flex flex-col md:flex-row items-center bg-slate-50 gap-2 rounded-lg p-4 shadow-sm">
+                <div className="flex flex-col w-1/6 h-full text-center p-4 bg-white rounded-md items-center">
+                    <div className="text-md font-bold">{day}</div>
+                    <div className="text-4xl text-emerald-400">{month}</div>
+                </div>
+                <span className="w-px h-full border-2 border-solid"></span>
+                <div className="flex flex row items-center gap-1">
+                    <IoMdTime className="h-6 w-6"/>
+                    <div className="text-lg">{a.time}</div>
+                </div>
+                <div className="w-full h-full text-center font-semibold">{a.fullname}</div>
+                <div className="w-full h-full flex flex-col items-center md:items-start">
+                    <div className="text-slate-600 font-semibold">Booked services:</div>
+                    <div className="flex flex-col lg:flex-row items-center">
+                        {bookedServices?.map((s, i) => (
+                            s.user_id === a.user_id ? <div key={i} className="w-full h-full text-center md:text-left ">{s.service_name}</div> : null
+                        ))}
+                    </div>
+                </div>
+            </div>
         ))}</div>
     </div>
 }
